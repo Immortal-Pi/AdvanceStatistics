1fec728a612eecbf202408953262788d5203b45f
unknown <26.amruth@gmail.com>
2024-06-22 17:23:33 +0530

    feature engineering

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('hello noobs')
+# from pandas_visual_analysis import VisualAnalysis
+# import seaborn as sns
+# from pandas_visual_analysis import VisualAnalysis
+# import numpy as np
+
+#
+# dataset=sns.load_dataset('iris')
+# VisualAnalysis(dataset)
